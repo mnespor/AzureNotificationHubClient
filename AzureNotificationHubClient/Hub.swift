bca8a3da96900b52e5fc78ebfcf9e0b14a575c38
mnespor <mnespor@gmail.com>
2016-02-10 22:51:31 -0600

    setup

---
+++ AzureNotificationHubClient/Hub.swift
@@ -0,0 +1,12 @@
+//
+//  Hub.swift
+//  AzureNotificationHubClient
+//
+//  Created by Matthew Nespor on 2/10/16.
+//  Copyright © 2016 Matthew Nespor. All rights reserved.
+//
+
+import Foundation
+
+public class Hub: NSObject {
+}
---
+++ testapp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  testapp
+//
+//  Created by Matthew Nespor on 2/10/16.
+//  Copyright © 2016 Matthew Nespor. All rights reserved.
+//
+
+import UIKit
+import AzureNotificationHubClient
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
