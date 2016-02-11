b51db7e314e9f0187daba7e81e1464c61996cb3c
mnespor <mnespor@gmail.com>
2016-02-10 22:40:11 -0600

    Initial Commit

---
+++ AzureNotificationHubClient/AzureNotificationHubClient.h
@@ -0,0 +1,19 @@
+//
+//  AzureNotificationHubClient.h
+//  AzureNotificationHubClient
+//
+//  Created by Matthew Nespor on 2/10/16.
+//  Copyright © 2016 Matthew Nespor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AzureNotificationHubClient.
+FOUNDATION_EXPORT double AzureNotificationHubClientVersionNumber;
+
+//! Project version string for AzureNotificationHubClient.
+FOUNDATION_EXPORT const unsigned char AzureNotificationHubClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AzureNotificationHubClient/PublicHeader.h>
+
+
