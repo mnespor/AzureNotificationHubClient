ac6b125c1e45f6d7c5fb37e8bd041822f5d20921
mnespor <mnespor@gmail.com>
2016-04-09 23:39:22 -0500

    feat: parseConnectionString

--- Pod/Classes/HubHelper.swift
+++ Pod/Classes/HubHelper.swift
@@ -23,4 +23,42 @@ internal class HubHelper {
 
         return NSURL(string: urlString)
     }
+
+    internal static func parseConnectionString(connectionString: String) -> [String: String] {
+        let validFieldPrefixes = ["endpoint==",
+                                  "sharedaccesskeyname=",
+                                  "sharedaccesskey=",
+                                  "sharedsecretissuer=",
+                                  "sharedsecretvalue=",
+                                  "stsendpoint="]
+
+        var connectionDictionary: [String: String] = [:]
+        let fields = connectionString.componentsSeparatedByString(";")
+        var previousLeft = ""
+        for i in 0..<fields.count {
+            if (i+1) < fields.count {
+                if validFieldPrefixes.indexOf({ prefix -> Bool in
+                    return fields[i].hasPrefix(prefix)
+                }) == nil {
+                    previousLeft = "\(previousLeft)\(fields[i])"
+                    continue
+                }
+            }
+
+            let currentField = "\(previousLeft)\(fields[i])"
+            previousLeft = ""
+            let keyValuePairs = currentField.componentsSeparatedByString("=")
+            guard keyValuePairs.count >= 2 else { break }
+
+            let keyName = keyValuePairs[0].lowercaseString
+            var keyValue = keyValuePairs[1]
+            if keyName == "endpoint" {
+                keyValue = HubHelper.url(NSURL(string: keyValue)!, withScheme: "https")?.absoluteString ?? ""
+            }
+
+            connectionDictionary[keyName] = keyValue
+        }
+
+        return connectionDictionary
+    }
 }
